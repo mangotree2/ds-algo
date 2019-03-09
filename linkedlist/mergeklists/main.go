89f942a30c78468f4c437f6411e561391c755790
mongotree2 <513129832@qq.com>
2019-03-09 18:27:08 +0800

    feat : 链表有环，合并k个有序链表

---
+++ linkedlist/mergeklists/main.go
@@ -0,0 +1,111 @@
+package main
+
+import "fmt"
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergetwolist(l1, l2 *ListNode) *ListNode {
+
+	if l1 == nil && l2 == nil {
+		return nil
+	}
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+
+
+	root := new(ListNode)
+	start := root
+	for {
+
+		if l1.Val <= l2.Val {
+			root.Next = l1
+			l1 = l1.Next
+
+		} else {
+			root.Next = l2
+			l2 = l2.Next
+		}
+		root = root.Next
+
+		if l1 == nil || l2 == nil {
+			break
+		}
+
+	}
+	if  l1== nil {
+		root.Next =l2
+	}
+	if l2 == nil {
+		root.Next = l1
+	}
+	//for l := start; l != nil ; l = l.Next{
+	//	fmt.Println(l.Val)
+	//}
+	return start.Next
+
+}
+
+func mergeKLists(lists []*ListNode) *ListNode {
+	if len(lists) == 0 {
+		return nil
+
+	}
+
+	if len(lists) == 1 {
+		return lists[0]
+	}
+
+	if len(lists) == 2 {
+		return mergetwolist(lists[0], lists[1])
+	}
+
+	lists = append(lists, mergetwolist(lists[0], lists[1]))
+	lists = lists[2:]
+
+
+	return mergeKLists(lists)
+
+}
+
+func main() {
+	l1 := &ListNode{
+		Val:  1,
+		Next: &ListNode{
+			Val:  4,
+			Next: &ListNode{
+				Val:  5,
+				Next: nil,
+			},
+		},
+	}
+
+	l2 := &ListNode{
+		Val:  1,
+		Next: &ListNode{
+			Val:  3,
+			Next: &ListNode{
+				Val:  4,
+				Next: nil,
+			},
+		},
+	}
+
+	l3 := &ListNode{
+		Val:  2,
+		Next: &ListNode{
+			Val:  6,
+			Next:nil,
+		},
+	}
+
+	for l := mergeKLists([]*ListNode{l1,l2,l3}); l != nil ;l = l.Next {
+		fmt.Println(l.Val)
+	}
+}
--- linkedlist/singlelinkedlist/cycle/cycle.go
+++ linkedlist/singlelinkedlist/cycle/cycle.go
@@ -6,6 +6,11 @@ import (
 	"github.com/mangotree2/ds-algo/linkedlist/singlelinkedlist"
 )
 
+//给定一个链表，判断链表中是否有环。
+//
+//为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+//
+//
 func main() {
 
 	l1 := list.New()
